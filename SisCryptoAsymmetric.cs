2db9aa07036402f52a8a52869e2bdfd31f11de1b
wffirilat <wffirilat@users.noreply.github.com>
2021-06-15 03:14:51 -0500

    Add stubs for AsymmetricSign

--- SisCryptoAsymmetric.cs
+++ SisCryptoAsymmetric.cs
@@ -7,5 +7,11 @@ public static partial class SisCrypto {
 
     public static AsymmetricEncryptResult AsymmetricEncrypt(byte[] publicKey, string data, int version = SISCRYPTO_LATEST_VERSION)
         => AsymmetricEncrypt(publicKey, StringToBytes(data, version), version);
+
+    public static AsymmetricSignResult AsymmetricSign(Secret<byte[]> privateKey, byte[] data, int version = SISCRYPTO_LATEST_VERSION)
+        => SisCryptoBackend.GetVersion(version).AsymmetricSign(privateKey, data);
+
+    public static AsymmetricSignResult AsymmetricSign(Secret<byte[]> privateKey, string data, int version = SISCRYPTO_LATEST_VERSION)
+        => AsymmetricSign(privateKey, StringToBytes(data, version), version);
 }
 }

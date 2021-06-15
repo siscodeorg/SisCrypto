eacaaadc15ba40ad4917586c7510ec155b201c44
wffirilat <wffirilat@users.noreply.github.com>
2021-06-15 03:17:36 -0500

    Add AsymmetricDecrypt stub

--- SisCryptoAsymmetric.cs
+++ SisCryptoAsymmetric.cs
@@ -13,5 +13,8 @@ public static partial class SisCrypto {
 
     public static AsymmetricSignResult AsymmetricSign(Secret<byte[]> privateKey, string data, int version = SISCRYPTO_LATEST_VERSION)
         => AsymmetricSign(privateKey, StringToBytes(data, version), version);
+
+    public static byte[] AsymmetricDecrypt(Secret<byte[]> privateKey, AsymmetricEncryptResult data, int version = SISCRYPTO_LATEST_VERSION)
+        => SisCryptoBackend.GetVersion(version).AsymmetricDecrypt(privateKey, data);
 }
 }
